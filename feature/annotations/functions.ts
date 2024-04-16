96c50a51a2214e4210788bfdb4bbdecbeca15b0b
RezaSoltani <59668273+reza6969@users.noreply.github.com>
2024-04-16 15:05:53 +0330

    Annotations for Anonymous Functions
    003 Annotations for Anonymous Functions

--- feature/annotations/functions.ts
+++ feature/annotations/functions.ts
@@ -1,3 +1,17 @@
-const add = (a: number, b: number): number => {
-  return a + b;
-};
+const add = (a: number, b: number): number => { return a + b; };
+
+const subtract = (a: number, b: number): number => { return a - b; };
+
+function divide(a: number, b: number): number { return a / b; };
+
+const multiply = function(a: number, b: number): number { return a * b; };
+
+export default class Calculator {
+  private _add: Function;
+  private _subtract: Function;
+
+  constructor(addFunction?: Function, subtractFunction?: Function) {
+    this._add = addFunction || add; //function() {}; // Default to no-op if not provided.
+    this._subtract = subtractFunction || subtract;
+  }
+}
