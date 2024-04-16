511bcde824a70d19f631d5328fc70303a5e7e258
r.soltani <infooffice1399@gmail.com>
2024-04-16 19:21:39 +0330

    Void and Never(04 - Annotations With Functions and Objects  )
    004 Void and Never

--- feature/annotations/functions.ts
+++ feature/annotations/functions.ts
@@ -6,6 +6,16 @@ function divide(a: number, b: number): number { return a / b; };
 
 const multiply = function(a: number, b: number): number { return a * b; };
 
+const logger = (message: string): void => { console.log(message); }
+
+const throwError = (message: string): void => { // never and string final void 
+  if(!message) {
+    throw new Error(message);
+  }
+
+  // return message
+}
+
 export default class Calculator {
   private _add: Function;
   private _subtract: Function;
