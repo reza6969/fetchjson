9ae40873ebea4df992594c30bf16a6e70e6b78ca
r.soltani <infooffice1399@gmail.com>
2024-04-16 19:38:20 +0330

    Destructuring with Annotations(04 - Annotations With Functions and Objects)
    005 Destructuring with Annotations

--- feature/annotations/functions.ts
+++ feature/annotations/functions.ts
@@ -16,6 +16,30 @@ const throwError = (message: string): void => { // never and string final void
   // return message
 }
 
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+// const logWeather = (forecast: { date: Date, weather: string }): void => {
+//   console.log(forecast.date);
+//   console.log(forecast.weather);
+// };
+
+
+//ES2015
+// const logWeather = ({ date, weather }) => {
+//   console.log(date);
+//   console.log(weather);
+// }
+
+const logWeather = ({date, weather}: { date: Date, weather: string }): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+
+logWeather(todaysWeather);
 export default class Calculator {
   private _add: Function;
   private _subtract: Function;
