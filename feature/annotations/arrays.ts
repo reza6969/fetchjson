274d5380c4ee5b96a498b6e2e233717f11748528
r.soltani <infooffice1399@gmail.com>
2024-04-21 00:07:43 +0330

    05 - Mastering Typed Arrays-001 Arrays in Typescript
    001 Arrays in Typescript

---
+++ feature/annotations/arrays.ts
@@ -0,0 +1,13 @@
+// const carMakers = ['ford', 'toyota', 'chevy' ]; // type interface
+// const carMakers: string[] = ['ford', 'toyota', 'chevy' ]; // type Annotations
+// case study
+const carMakers: string[] = []; // initialize the array
+
+const dates = [new Date(), new Date()];
+
+// const carByMake = [
+//   ['f150'],
+//   ['corolla'],
+//   ['camaro']
+// ];
+const carByMake: string[][] = [];
