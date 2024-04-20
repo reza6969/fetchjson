9e7c885467f0c9caf00e4cd9cc44f9196bf91f93
r.soltani <infooffice1399@gmail.com>
2024-04-20 23:40:08 +0330

    04-Annotations With Functions and Objects-006-Annotations Around Objects
    006 Annotations Around Objects

---
+++ feature/annotations/objects.ts
@@ -0,0 +1,16 @@
+const profile = {
+  name: 'alex',
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+  }
+};
+
+const age1 = profile.age;
+// const { age }: { age: number } = profile;
+const { age, name }: { age: number; name: string } = profile;
+const { coords: { lat, lng } }: { coords: { lat: number; lng: number}} = profile;
