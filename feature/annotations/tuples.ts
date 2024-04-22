171849b39263d179c75a8d100bb2f7e8229459b6
r.soltani <infooffice1399@gmail.com>
2024-04-22 19:54:46 +0330

    06 - Tuples in Typescript-003 Why Tuples
    003 Why Tuples

--- feature/annotations/tuples.ts
+++ feature/annotations/tuples.ts
@@ -14,3 +14,10 @@ pepsi[0] = 40 ;
 type Drink = [string, boolean, number];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+}
