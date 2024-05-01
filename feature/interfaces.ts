d06bc3107cf3586d0168b0036d50a2c49bb8ba81
r.soltani <infooffice1399@gmail.com>
2024-05-02 00:55:59 +0330

    07 - The All-Important Interface-004 Syntax Around Interfaces
    004 Syntax Around Interfaces

--- feature/interfaces.ts
+++ feature/interfaces.ts
@@ -1,13 +1,19 @@
 interface Vehicle {
   name: string;
-  year: number;
+  // year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  // year: 2000,
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`
+  }
 };
 
 // const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
@@ -20,5 +26,6 @@ const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Broken? ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 printVehicle(oldCivic);
