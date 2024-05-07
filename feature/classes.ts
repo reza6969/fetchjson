78e06cb8a09bb8547c58de2d71fee33defc04e64
r.soltani <infooffice1399@gmail.com>
2024-05-08 00:09:59 +0330

    08 - Building Functionality with Classes-004
    004 Fields in Classes

--- feature/classes.ts
+++ feature/classes.ts
@@ -1,4 +1,11 @@
 class Vehicle {
+  // color: string = 'red';
+  // color: string;
+
+  // constructor(color: string) {
+  //   this.color = color;
+  // }
+  constructor(public color: string) {}
   // public drive(): void {
   //   console.log('chugga chugga');
   // }
@@ -8,23 +15,23 @@ class Vehicle {
   }
 }
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log('vroom');
-  }
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log('vroom');
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const vehicle = new Vehicle();
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color)
 // vehicle.drive();
-
 // Property 'honk' is protected and only accessible within class 'Vehicle' and its subclasses.
 // vehicle.honk();
 
-const car = new Car();
-car.startDrivingProcess();
+// const car = new Car();
+// car.startDrivingProcess();
 // car.honk();
